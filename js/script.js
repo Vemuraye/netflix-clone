5396a509bc826e89c2863b86f9785de74c39dd32
vemuraye <vemuraye@gmail.com>
2022-08-23 20:42:43 +0100

    Init

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+    removeborder();
+    removeShow();
+    // Add border to current tab
+    this.classList.add('tab-border');
+    // Grab content item from DOM
+    const tabContentItems = document.querySelector(`#${this.id}-content`);
+    // Add show class
+    tabContentItems.classList.add('show')
+}
+
+function removeborder () {
+    tabItems.forEach(item => item.classList.remove('tab-border'))
+}
+
+function removeShow () {
+    tabContentItems.forEach(item => item.classList.remove('show'))
+}
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem))
+
+''
+""
+``
